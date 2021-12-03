a0c80cb340afd46fdaefaa5cb3949f4c6dd42d8e
Luc Secouard <luc.secouard@laposte.net>
2021-12-03 20:40:20 +0100

    Add files via upload

---
+++ Day 02/p02.nim
@@ -0,0 +1,35 @@
+import strutils
+
+type Command {.pure.} = enum Forward = "forward", Up = "up", Down = "down"
+
+# Read commands.
+var commands: seq[tuple[command: Command, value: int]]
+for line in lines("p02.data"):
+  let fields = line.split()
+  commands.add (parseEnum[Command](fields[0]), parseInt(fields[1]))
+
+# Part 1.
+var pos, depth = 0
+for (command, value) in commands:
+  case command
+  of Forward: inc pos, value
+  of Up: dec depth, value
+  of Down: inc depth, value
+
+echo "Part 1 answer: ", pos * depth
+
+# Part 2.
+pos = 0
+depth = 0
+var aim = 0
+for (command, value) in commands:
+  case command
+  of Forward:
+    inc pos, value
+    inc depth, aim * value
+  of Up:
+    dec aim, value
+  of Down:
+    inc aim, value
+
+echo "Part 2 answer: ", pos * depth
