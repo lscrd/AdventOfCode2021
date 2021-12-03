1ef54b28b6c725f38def217d33e6f68f559720ef
Luc Secouard <luc.secouard@laposte.net>
2021-12-03 20:39:48 +0100

    Add files via upload

---
+++ Day 01/p01.nim
@@ -0,0 +1,24 @@
+import strutils, sugar
+
+# Read values.
+let values = collect:
+               for line in lines("p01.data"): parseInt(line)
+
+# Part 1.
+var prevVal = values[0]
+var count = 0
+for i in 1..values.high:
+  if values[i] > prevVal: inc count
+  prevVal = values[i]
+
+echo "Part 1 answer: ", count
+
+# Part 2.
+prevVal = values[0] + values[1] + values[2]
+count = 0
+for i in 3..values.high:
+  let val = values[i-2] + values[i-1] + values[i]
+  if val > prevVal: inc count
+  prevVal = val
+
+echo "Part 2 answer: ", count
