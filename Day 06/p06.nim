6e2d68561c26ab28fb99486689a82925f3f6f563
Luc Secouard <luc.secouard@laposte.net>
2021-12-07 09:27:47 +0100

    Add files via upload

---
+++ Day 06/p06.nim
@@ -0,0 +1,25 @@
+import algorithm, math, strutils
+
+# Number of lanternfishes for each remaining days.
+type Count = array[0..8, int]
+
+let input = open("p06.data")
+var lanternFishCount: Count
+for value in input.readLine().split(','):
+  lanternFishCount[parseInt(value)].inc
+input.close()
+
+proc doStep(lanternFishCount: var Count) =
+  ## Update the counts after one day.
+  lanternFishCount.rotateLeft(1)
+  lanternFishCount[6].inc lanternFishCount[8]
+
+# Part 1.
+for _ in 1..80:
+  lanternFishCount.doStep()
+echo "Part 1 answer: ", sum(lanternFishCount)
+
+# Part 2.
+for _ in 81..256:
+  lanternFishCount.doStep()
+echo "Part 2 answer: ", sum(lanternFishCount)
