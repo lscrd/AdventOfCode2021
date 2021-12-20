05244acd5fad59e064dc71dfac0968590109c3b8
Luc Secouard <luc.secouard@laposte.net>
2021-12-20 10:18:33 +0100

    Add files via upload
    Modified algorithm about 9 times faster than the original one.

--- Day 19/p19.nim
+++ Day 19/p19.nim
@@ -5,8 +5,9 @@ type
   # Coordinates and shifts.
   Vector = array[3, int]
 
-  # List of beacons.
+  # List and set of beacons.
   Beacons = seq[Vector]
+  BeaconSet = HashSet[Vector]
 
   # Scanner data.
   ScannerData = seq[Beacons]
@@ -33,37 +34,38 @@ for line in lines("p19.data"):
     discard line.scanf("$i,$i,$i", coords[0], coords[1], coords[2])
     data[^1].add coords
 
+
 # Addition and subtraction of vectors.
 func `+`(a, b: Vector): Vector = [a[0] + b[0], a[1] + b[1], a[2] + b[2]]
 func `-`(a, b: Vector): Vector = [a[0] - b[0], a[1] - b[1], a[2] - b[2]]
+
 
 func rotated(v, perm, mult: Vector): Vector =
   ## Return the coordinates of a vector rotated using a permutation and a multiplication vectors.
   [v[perm[0]] * mult[0], v[perm[1]] * mult[1], v[perm[2]] * mult[2]]
 
-iterator rotations(beacons: Beacons): tuple[beacons: Beacons; perm, mult: array[3, int]] =
-  ## Yield the coordinates of a list of beacons after applying the successive rotations.
+
+iterator rotations(): tuple[perm, mult: Vector] =
+  ## Yield the successive permutations.
   for (perm, mults) in Rotations:
     for mult in mults:
-      var rotated: Beacons
-      for beacon in beacons:
-        rotated.add beacon.rotated(perm, mult)
-      yield (rotated, perm, mult)
+      yield (perm, mult)
 
-proc compare(b1, b2: Beacons): tuple[match: bool; shift, perm, mult: Vector] =
-  ## Compare two list of beacons and return a tuple indicating if a match has been found
-  ## and, if this is the case, the shift and the rotation to use for the second list of beacons.
+
+proc find(b1: BeaconSet; b2: Beacons): tuple[match: bool; shift, perm, mult: Vector] =
+  ## Compare a list of beacons with a beacon set and return a tuple indicating if a match has
+  ## been found and, if this is the case, the shift and the rotation to use for the list of beacons.
   var shift: Vector
-  for i0 in 0..(b1.len - 12):
-    let v1 = b1[i0]
-    for (r2, perm, mult) in b2.rotations():
-      let s2 = r2.toHashSet()   # To speed up the tests.
-      for v2 in r2:
+  var b1 = b1
+  for i0 in 0..(b2.len - 12):
+    for (perm, mult) in rotations():
+      let v2 = b2[i0].rotated(perm, mult)
+      for v1 in b1:
         shift = v2 - v1
         var count = 1
-        for i in (i0 + 1)..b1.high:
-          if b1[i] + shift in s2:
-            # The shifted position belongs to the set of "b2" rotated positions.
+        for i in (i0 + 1)..b2.high:
+          if b2[i].rotated(perm, mult) - shift in b1:
+            # The rotated and shifted position belongs to the known set of beacons.
             inc count
             if count == 12:
               return (true, shift, perm, mult)
@@ -72,14 +74,14 @@ proc compare(b1, b2: Beacons): tuple[match: bool; shift, perm, mult: Vector] =
 proc process(data: ScannerData) =
   ## Process data and print results for parts 1 and 2.
 
-  # Build the list of beacons.
+  # Build the set of beacons.
   var perms, mults, shifts = newSeq[Vector](data.len)   # Description of scanner positions.
   perms[0] = [0, 1, 2]
   mults[0] = [1, 1, 1]
   shifts[0] = [0, 0, 0]
-  var beacons = data[0]                         # Start with data from scanner 0.
+  var beacons = data[0].toHashSet()                     # Start with data from scanner 0.
   var scannersToProcess = toSeq(1..data.high)
-  # - find a scanner with at least 12 shared beacons.
+  # - find a scanner with at least 12 common beacons.
   while scannersToProcess.len > 0:
     var match = false
     var shift, perm, mult: Vector
@@ -88,17 +90,15 @@ proc process(data: ScannerData) =
     while not match:
       inc idx
       scanner = scannersToProcess[idx]
-      (match, shift, perm, mult) = compare(beacons, data[scanner])
+      (match, shift, perm, mult) = beacons.find(data[scanner])
     # - update position data for this scanner.
     perms[scanner] = perm
     mults[scanner] = mult
     shifts[scanner] = shift
     scannersToProcess.del idx
-    # Add beacons. We use an intermediate set to speed up detection of duplicates.
-    var beaconSet = beacons.toHashSet()
+    # Add beacons from this scanner.
     for beacon in data[scanner]:
-      beaconSet.incl beacon.rotated(perm, mult) - shift
-    beacons = toSeq(beaconSet.items)
+      beacons.incl beacon.rotated(perm, mult) - shift
 
   echo "Part 1 answer: ", beacons.len
 
