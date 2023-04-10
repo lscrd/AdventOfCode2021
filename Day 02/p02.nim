bb70cb8a61a3e07268acf9e1a2f1b644eb4ddd40
Luc Secouard <luc.secouard@laposte.net>
2023-04-10 14:07:24 +0200

    Update p02.nim

--- Day 02/p02.nim
+++ Day 02/p02.nim
@@ -8,7 +8,8 @@ for line in lines("p02.data"):
   let fields = line.split()
   commands.add (parseEnum[Command](fields[0]), parseInt(fields[1]))
 
-# Part 1.
+### Part 1 ###
+
 var pos, depth = 0
 for (command, value) in commands:
   case command
@@ -16,9 +17,10 @@ for (command, value) in commands:
   of Up: dec depth, value
   of Down: inc depth, value
 
-echo "Part 1 answer: ", pos * depth
+echo "Part 1: ", pos * depth
 
-# Part 2.
+### Part 2 ###
+
 pos = 0
 depth = 0
 var aim = 0
@@ -32,4 +34,4 @@ for (command, value) in commands:
   of Down:
     inc aim, value
 
-echo "Part 2 answer: ", pos * depth
+echo "Part 2: ", pos * depth
